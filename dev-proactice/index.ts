a359ce7244b4a3342200ec9b640c53c50300d55c
Gwenaël Gallon <gwenael.gallon@mac.com>
2022-12-03 00:44:51 +0100

    Fix up

--- dev-proactice/index.ts
+++ dev-proactice/index.ts
@@ -1,7 +1,7 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
+    context.log('HTTP trigger function processed a request :)');
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
         ? "Hello, " + name + ". This HTTP triggered function executed successfully."
